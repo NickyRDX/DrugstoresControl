55d3c174da5e93c564ff1b23b669acb4c306b512
Nicky <NickyRDX@hotmail.com>
2026-03-15 17:57:02 -0300

    agregando graficos de margenes de ganancias que optiene el drugstores

--- app/(routes)/(principal)/dashboard/page.tsx
+++ app/(routes)/(principal)/dashboard/page.tsx
@@ -1,14 +1,22 @@
 "use client";
 import CardComponent from "@/shared/components/CardComponent/CardComponent";
+import { ChartAreaStacked } from "@/shared/components/ChartStacked";
 import React from "react";
 
 export default function DashboardPage() {
   return (
-    <section className="p-6 gap-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-[1600px] mx-auto">
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
+    <section className="flex flex-col gap-8 p-6 max-w-[1600px] mx-auto w-full">
+      <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+        <div>
+          <CardComponent />
+        </div>
+        <CardComponent />
+        <CardComponent />
+        <CardComponent />
+      </section>
+      <section className="gap-6">
+        <ChartAreaStacked />
+      </section>
     </section>
   );
 }
